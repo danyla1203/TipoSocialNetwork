a69abf36fd0bdd496384ca7ffc239bebcb7f229c
Daniil <zelenko.danil@gmail.com>
2020-05-17 10:35:06 +0300

    create friends model. Integrated

--- server/friends.js
+++ server/friends.js
@@ -1,32 +1,17 @@
-const app = require("./server").app;
-const upload = require("./server").upload;
-const pool = require("./server").pool;
-const makeSql = require("./server").makeSql;
+const app = require("./index").app;
+const friendsModel = require("./index").friendsModel;
 
 app.get("/data/friends", (req, res) => {
-    let sql = makeSql
-                .select(["id", "user_id", "avatar_url_icon", "name"])
-                .from("friends")
-                .join("users")
-                .on("friends.user2_id = users.user_id")
-                .where(`user1_id = ${req.session.user.user_id}`);
-                
-    pool.query(sql, (err, result) => {
+    friendsModel.getFriends(req.session.user_id, (err, result) => {
         if (err) throw err;
         res.end(JSON.stringify(result));
-    });
+    })    
 })
 
 app.get("/data/is-friend/:user2_id", (req ,res) => {
-    let sql = makeSql
-                .select()
-                .from("friends")
-                .where(`user1_id = ${req.session.user.user_id} AND user2_id = ${req.params.user2_id}`);
-
-
-    pool.query(sql, (err, result) => {
+    friendsModel.isFriend(req.session.user.user_id, req.params.user2_id, (err, result) => {
         if (err) throw err;
-    
+        
         if (result.length > 0) {
             res.end("true");
         } else {
@@ -36,41 +21,15 @@ app.get("/data/is-friend/:user2_id", (req ,res) => {
 });
 
 app.get("/data/add/friends/:user_id", (req, res) => {
-    let sql = makeSql
-                .select()
-                .from("friends")
-                .where(`user1_id = ${req.session.user.user_id}`);
-
-    pool.query(sql, (err, result) => {
+    friendsModel.addFriend(req.session.user.user_id, req.params.user_id, (err, result) => {
         if (err) throw err;
-    
-        for (let i = 0; i < result.length; i++) {
-            if (parseInt(result[i].user2_id) == parseInt(req.params.user_id)) {
-                res.end("User already your friend");
-                return; 
-            }
-        }
-
-        let sql = makeSql
-                    .insert("friends")
-                    .set({
-                        user1_id: req.session.user.user_id,
-                        user2_id: req.params.user_id    
-                    })
-        pool.query(sql, (err) => {
-            if (err) throw err;
-            res.end("Friend added");
-        });
+        res.end(result)
     })
 });
 
 app.get("/data/delete/friends/:user_id", (req, res) => {
-    let sql = makeSql
-                .delete("friends")
-                .where(`user1_id = ${req.session.user.user_id} AND user2_id = ${req.params.user_id}`);
-    
-    pool.query(sql, (err) => {
+    friendsModel.deleteFriend(req.session.user.user_id, req.params.user_id, (err, result) => {
         if (err) throw err;
-        res.end();
-    })
+        res.end("deleted");
+    });
 })
---
+++ server/models/FriendsModel.js
@@ -0,0 +1,59 @@
+const Model = require("./Model");
+
+class FriendsModel extends Model {
+    getFriends(user_id, callback) {
+        let sql = this.sqlMaker
+                .select(["id", "user_id", "avatar_url_icon", "name"])
+                .from("friends")
+                .join("users")
+                .on("friends.user2_id = users.user_id")
+                .where(`user1_id = ${user_id}`);
+                
+        this.pool.query(sql, callback);
+    }
+
+    isFriend(user1_id, user2_id, callback) {
+        let sql = this.sqlMaker
+                .select()
+                .from("friends")
+                .where(`user1_id = ${user1_id} AND user2_id = ${user2_id}`);
+
+        pool.query(sql, callback);
+    }
+    
+    addFriend(user1_id, user2_id, callback) {
+        let isFriend;
+        this.getFriends(user1_id, (err, result) => {
+            if (err) throw err;
+            for (let i = 0; i < result.length; i++) {
+                if (parseInt(result[i].user2_id) == parseInt(user2_id)) {
+                    isFriend = true;
+                    return; 
+                }
+            }
+        });
+
+        if (!isFriend) { 
+            let sql = this.sqlMaker
+                .insert("friends")
+                .set({
+                    user1_id: user1_id,
+                    user2_id: user2_id    
+                })
+            
+            this.pool.query(sql, callback);
+        } else {
+            callback("User is your friend already");
+        }
+    }
+
+    deleteFriend(user1_id, user2_id, callback) {
+        let sql = this.sqlMaker
+            .delete("friends")
+            .where(`user1_id = ${user1_id} AND user2_id = ${user2_id}`);
+    
+        pool.query(sql, callback);
+    }
+}
+
+module.exports.FriendsModel = FriendsModel;
