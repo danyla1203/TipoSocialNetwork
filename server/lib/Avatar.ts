bf6386294e2c156372ff2660b2733d2914e00c22
Daniil <zelenko.danil@gmail.com>
2020-05-29 19:05:22 +0300

    Avatar to ts

--- server/lib/Avatar.ts
+++ server/lib/Avatar.ts
@@ -2,23 +2,21 @@ const fs = require("fs");
 const path = require("path");
 const sharp = require("sharp");
 
-class Avatar {
-    deleteOldAvatars(id) {
+export class Avatar {
+    deleteOldAvatars(id: number) {
         let path = `/home/daniil/Desktop/NodeProjects/AuthTest/public/img/${id}`;
-        fs.unlink(`${path}_icon.webp`, (err) => {
+        fs.unlink(`${path}_icon.webp`, (err: Error) => {
             if (err) { console.log("user have a default icon"); }
         });
-        fs.unlink(`${path}_full.webp`, (err) => {
+        fs.unlink(`${path}_full.webp`, (err: Error) => {
             if (err) { console.log("user have a default icon"); }
         });
     }
 
-    makeAvatar(filePath, id) {
+    makeAvatar(filePath: string, id: number) {
         let targetPath_icon = path.join("/home/daniil/Desktop/NodeProjects/AuthTest", "/public/img/" + id + "_icon" + ".webp");
         let targetPath_full = path.join("/home/daniil/Desktop/NodeProjects/AuthTest", "/public/img/" + id + "_full" + ".webp");
         sharp(filePath).resize(70, 80).toFile(targetPath_icon);
         sharp(filePath).resize(300, 300).toFile(targetPath_full);
     }
 }
-
-module.exports = Avatar;
