a19c33b996c123ba590de79041529f605bfeac87
Daniil <zelenko.danil@gmail.com>
2020-07-18 13:06:37 +0300

    factory update

--- server/lib/Factory.ts
+++ server/lib/Factory.ts
@@ -1,24 +1,34 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, Application } from "express";
 import "reflect-metadata";
 
 import { App } from "../app.module";
+import { Module } from "./Module";
 
 export class Factory {
-    
-    createApp() {
-        let startModule = new App();
-        let app = express();
-        
-        for(let i = 0; i < startModule.modules.length; i++) {
-            let moduleController = startModule.modules[i]["controller"];
+    setHandlers(modules: Module[], app: Application): void {
+        for(let i = 0; i < modules.length; i++) {
+            let moduleController = modules[i]["controller"];
             let metadataKeys = Reflect.getMetadataKeys(moduleController);
             for(let j = 0; j < metadataKeys.length; j++) {
                 let key = metadataKeys[j];
                 let metadataValue = Reflect.getMetadata(key, moduleController);
-
-                app.get(metadataValue[0], metadataValue[1]);
+                
+                let handler = metadataValue[1].bind(moduleController);
+                let path = metadataValue[0];
+                app.get(path, handler);
             }
         }
+    }
+
+    setMiddlewares(app: Application) {
+        
+    }
+
+    createApp(): Application {
+        let startModule = new App();
+        let app = express();
+        
+        this.setHandlers(startModule.modules, app);
         return app;
     }
 }
